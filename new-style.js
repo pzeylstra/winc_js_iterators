c298bfd645a2dc7ba107a033495670d7a6de5add
Paméla Zeylstra <pamela@pam-organisatieondersteuning.nl>
2021-12-10 14:34:36 +0100

    Add files via upload

---
+++ new-style.js
@@ -0,0 +1,7 @@
+const colors = ["yellow", "blue", "red", "orange"];
+colors.forEach(colors => console.log(colors));
+
+
+// How many lines does my for loop and my while loop take? For 3 While 5
+// How many lines does my forEach method take? 2
+// What other advantages do using an array method have over a for or a while loop? Kan ik niet verklaren, want heb ik onvoldoende over onderwezen gekregen
---
+++ old-style.js
@@ -0,0 +1,11 @@
+const colors = ["yellow", "blue", "red", "orange"]
+
+let i = 0;
+while (i < colors.length) {
+    console.log(colors[i]);
+    i++;
+}
+
+for (i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
